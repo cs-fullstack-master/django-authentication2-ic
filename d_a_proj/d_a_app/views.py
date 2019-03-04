b9ef544592cb41bad541bf6031961a7fc469dbff
Kevin Yancy <kevin@code-crew.org>
2019-03-04 14:31:41 -0600

    Updates

--- d_a_proj/d_a_app/views.py
+++ d_a_proj/d_a_app/views.py
@@ -1,11 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+@login_required
 def all_classes(request):
-    return render(request, 'd_a_app/all_classes.html')
+    course_list = Course.objects.all()
+    context = {'course_list': course_list}
+    return render(request, 'd_a_app/all_classes.html', context)
 
+@login_required
 def my_classes(request):
-    return render(request, 'd_a_app/my_classes.html')
+    course_list = Course.objects.filter(course_teacher=request.user)
+    context = {'course_list': course_list}
+    return render(request, 'd_a_app/my_classes.html',context)
 
 
